4872a2788f0697029d0d77d965a80f48e1178149
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2025-11-04 15:22:06 +0300

    cors fix

--- middleware.js
+++ middleware.js
@@ -1,20 +1,23 @@
-// middleware.js
-export function middleware(req) {
-  const res = new Response(null, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  });
+import { NextResponse } from 'next/server';
 
+export function middleware(req) {
   if (req.method === 'OPTIONS') {
-    return res;
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
   }
 
-  return Response.next();
+  const res = NextResponse.next();
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return res;
 }
 
 export const config = {
-  matcher: '/api/:path*', // sadece API route'larını kapsar
+  matcher: '/api/:path*',
 };
